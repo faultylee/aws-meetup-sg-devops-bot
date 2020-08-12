ca98e6d06bfaf2bde96c8f48fa18575d6ff471da
faulty <faulty.lee@gmail.com>
2020-08-12 13:52:19 +0800

    moved server code into src

--- server/config.sample.py
+++
@@ -1,7 +0,0 @@
-import os
-
-AWS_ACCOUNT_ID = ''
-AWS_REGION = os.environ['AWS_REGION']
-API_GATEWAY_ROOT = 'API_GATEWAY_ID.execute-api.ap-southeast-1.amazonaws.com'
-DDB_TABLE_NAME = 'devops-bot-client'
-WEB_HOOK_FUNCTION_NAME = ''
